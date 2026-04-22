cd0ce8a6c7eca0cfc530a13aa487b5c33ee7f382
BritonCodex <britonkiptoo1@gmail.com>
2026-04-22 13:33:45 +0300

    Mod: auth screens

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AuthLayout = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Auth Layout</Text>
+    </View>
+  );
+};
+
+export default AuthLayout;
